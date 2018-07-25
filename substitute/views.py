9bc0072ffca9c6d0c660acbed3dc4540680b693c
Florian <florian.lescuyer@gmail.com>
2018-07-25 18:03:29 +0200

    add context in view 'results'

--- substitute/views.py
+++ substitute/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .libs.api_interactions import DataProviderClient
+
+
 def index(request):
-    return HttpResponse("Page d'accueil de Pur Beurre")
+    """ Home page of Pur Beurre """
+
+    return render(request, "substitute/index.html", locals())
+
+def results(request):
+    """ Page with results of a request """
+
+    query = request.GET.get("query")
+    user_request = DataProviderClient()
+    substitutes_request = user_request.get_substitutes(query)
+    substitutes = []
+    for product in substitutes_request:
+        substitute = {
+            "name": product.get("product_name_fr"),
+            "nutriscore": product["nutrition_grade_fr"],
+            "picture": product.get("image_url", ""),
+            "id_product": product["code"]
+        }
+        substitutes.append(substitute)
+    context = {
+        "query": query,
+        "substitutes": substitutes
+    }
+    return render(request, "substitute/results.html", context)
+    
