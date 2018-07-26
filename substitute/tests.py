ae75a241ff9e3fb59cd2daa4a7a7135a15823421
Florian <florian.lescuyer@gmail.com>
2018-07-26 16:33:01 +0200

    update tests.py with the two classes for data

--- substitute/tests.py
+++ substitute/tests.py
@@ -1,59 +1,14 @@
 from django.test import TestCase
 import requests
 
-# from .models import Category, Product
-
-
-def _get_data_from_api(query, nutriscore=""):
-    """
-    This function search a product in Open Food Facts from the query of the user
-    """
-
-    payload = {
-        "search_terms": query,
-        "action": "process",
-        "nutrition_grades": nutriscore,
-        "search_simple": 1,
-        "json": 1,
-    }
-    search = requests.get(
-                    "https://fr.openfoodfacts.org/cgi/search.pl",
-                    params=payload
-                    )
-    data = search.json()
-    return data
-
-def get_category(query):
-    """
-    This function get the first french category of a product from the first
-    result of a search
-    """
-
-    data = _get_data_from_api(query)
-    # I choose the first product in the list of results
-    product = data["products"][0]
-    # I choose the first category write in french (rule defines by myself)
-    # The name of category looks like "fr:category-name"
-    for category in product["categories_tags"]:
-        if "fr" in category:
-            french_category = category
-            break
-    # I just want to keep the string after "fr:"
-    french_category = french_category[3:]
-    french_category = french_category.replace("-", " ")
-    return french_category
-
-def get_substitutes(query):
-    """
-    This function get a list of substitues from a category of a sought product
-    with the nutriscore A.
-    """
-    category = get_category(query)
-    substitutes = _get_data_from_api(category, "a")
-    return substitutes
+from .libs.api_interactions import DataApiClient, Substitutes
 
 
 class SearchAlgorithmeTestCase(TestCase):
+
+    def setUp(self):
+        self.user_request = DataApiClient()
+        self.substitutes = Substitutes("")
     
     def test_api_returns_status_code_200(self):
         res = requests.get("https://fr.openfoodfacts.org/cgi/search.pl")
@@ -62,17 +17,37 @@ class SearchAlgorithmeTestCase(TestCase):
     # Recherche de produit dans OFF
     # Tester le format de la réponse de l'API (contient les élémént dont on a besoin)
     def test_api_returns_list_of_products(self):
-        data = _get_data_from_api("nutella")
-        self.assertTrue(data["products"])
+        self.user_request.query = "nutella"
+        data = self.user_request.get_data_from_api()
+        self.assertTrue(len(data) > 0)
 
     # Tester qu'une catégorie est extraite de la réponse de l'API
     def test_get_category_from_product(self):
-        category = get_category("nutella")
-        self.assertEqual(category, "pates a tartiner")
+        self.substitutes.query = "nutella"
+        category = self.substitutes._get_category()
+        self.assertEqual(category, "Pâtes à tartiner")
 
     # Recherche de substituts
-    def test_get_substitutes_returns_list_of_products(self):
-        substitutes = get_substitutes("nutella")
-        self.assertTrue(substitutes["products"])
-        
- 
+    def test_get_substitutes_returns_products_with_nutella(self):
+        self.substitutes.query = "nutella"
+        substitutes_list = self.substitutes.get_substitutes()
+        self.assertEqual(len(substitutes_list), 5)
+    
+    def test_get_substitutes_not_returns_products_with_petit_ecolier(self):
+        self.substitutes.query = "petit écolier"
+        substitutes_list = self.substitutes.get_substitutes()
+        self.assertEqual(len(substitutes_list), 7)
+    
+    def test_app_return_substitutes(self):
+        products = ["nutella", "coca-cola", "belvita", "eau de source", "granola", "jambon", "prince", "kiri", "eau minérale gazeuse", "ketchup", "muesli", "moutarde de dijon", "ice tea", "chocolat au lait", "pain de mie", "ricoré", "pringles", "biscuits sésame", "Oreo", "tagada", "thon entier au naturel", "lait", "knaki", "petit beurre", "nesquik", "jus d'orange", "lardons", "coco pops", "cracottes", "oasis", "activia", "yaourt aux fruits", "chavroux", "fromage", "perrier", "crunch", "tomacouli", "ebly", "m&ms", "chocapic", "st hubert", "margarine", "lasagne bolognaise", "danette", "ravioli boeuf", "coquillettes", "mousline", "steak haché", "pain au lait", "confiture framboise"]
+        count = 0
+        no_results_products = []
+        for product in products:
+            self.substitutes.query = product
+            substitutes_list = self.substitutes.get_substitutes()
+            if len(substitutes_list) > 0: 
+                count += 1
+            else:
+                no_results_products.append(product)
+        print(len(products), count, len(no_results_products), no_results_products)
+        self.assertTrue(count > 42)
