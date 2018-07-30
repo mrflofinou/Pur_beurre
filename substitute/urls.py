ae422b4cf6c2b6d5ab5cb1d9355356085e89ce52
Florian <florian.lescuyer@gmail.com>
2018-07-30 18:55:56 +0200

    update url pattern of the details view

--- substitute/urls.py
+++ substitute/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.results, name="results"),
-    url(r'^details/$', views.details, name="details")
+    url(r'^(?P<product_id>[0-9]+)/$', views.details, name="details")
 ]
