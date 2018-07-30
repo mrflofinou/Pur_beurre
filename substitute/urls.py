cf967733d6525241bb2f91ef75a8fb38a1cdb19d
Florian <florian.lescuyer@gmail.com>
2018-07-30 16:29:46 +0200

    Add url pattern for details page

--- substitute/urls.py
+++ substitute/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.results, name="results")
+    url(r'^$', views.results, name="results"),
+    url(r'^details/$', views.details, name="details")
 ]
