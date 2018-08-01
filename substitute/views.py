934195a83ca8697ff85d61cc482e18a6068bfa9f
Florian <florian.lescuyer@gmail.com>
2018-08-01 18:04:10 +0200

    Modification of views results and details to use session variable

--- substitute/views.py
+++ substitute/views.py
@@ -8,16 +8,16 @@ from .libs.api_interactions import Substitutes
 
 def index(request):
     """ Home page of Pur Beurre """
-
     return render(request, "substitute/index.html", locals())
 
 def results(request):
     """ Page with results of a request """
 
     query = request.GET.get("query")
-    # user_request = DataProviderClient(query=query)
-    substitutes = Substitutes(query)
+    substitutes = Substitutes(query=query)
     substitutes_request = substitutes.get_substitutes()
+    # I use session to store the results of the user query
+    request.session["substitutes"] = substitutes_request
     substitutes_list = []
     for product in substitutes_request:
         substitute = {
@@ -36,13 +36,16 @@ def results(request):
 def details(request, product_id):
     """ Page with details of a selected substitute """
 
-    substitutes_list = json.load(open("substitutes.json"))
-    for substitute in substitutes_list:
-        if substitute["_id"] == product_id:
+    # I use substitues store in the session
+    for substitute in request.session["substitutes"]:
+        if substitute["code"] == product_id:
             context = {
                 "name": substitute.get("product_name_fr"),
                 "nutriscore": substitute["nutrition_grade_fr"],
-                "picture": substitute.get("image_url", ""),
+                "picture": substitute.get("image_url"),
+                "ingredients": substitute.get("ingredients_text_with_allergens_fr"),
+                "nutrition_picture": substitute.get("image_nutrition_url", ""),
+                "stores": substitute.get("stores")
             }
             break
     return render(request, "substitute/details.html", context)
