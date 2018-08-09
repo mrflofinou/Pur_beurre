cb504a64f0699a1ee45a6b7a37eed180f20d4eed
Florian <florian.lescuyer@gmail.com>
2018-08-09 18:43:10 +0200

    add test for details page

--- substitute/tests/functional_tests.py
+++ substitute/tests/functional_tests.py
@@ -1,46 +1,65 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
 
+import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.support import expected_conditions as ec
 
 
-class FonctionalTest(TestCase):
+class FunctionalTest(LiveServerTestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.get("http://localhost:8000/")
-    
+        self.wait = ui.WebDriverWait(self.driver, 1000)
+
+
     def tearDown(self):
         self.driver.quit()
+    
+    def user_makes_search(self):
+        element = self.driver.find_element_by_id("foodsearch")
+        submit = self.driver.find_element_by_id("submit")
+        element.send_keys("nutella")
+        submit.click()
+        self.wait.until(ec.presence_of_element_located((By.ID, "substitutes")))
+
+    def test_home_page_returns_200(self):
+        res = requests.get(self.driver.current_url)
+        self.assertEqual(res.status_code, 200)
 
     def test_Pur_Beurre_in_title_of_home_page(self):
         self.assertIn("Pur Beurre", self.driver.title)
     
-    def test_when_make_food_search_results_page_appears(self):
-        element = self.driver.find_element_by_id("foodsearch")
-        submit = self.driver.find_element_by_id("submit")
-        element.send_keys("nutella")
-        submit.click()
-        self.assertEqual(self.driver.current_url, "http://localhost:8000/substitute/?query=nutella")
+    def test_results_page_returns_200(self):
+        self.user_makes_search()
+        res = requests.get(self.driver.current_url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_results_page_url_contains_query(self):
+        self.user_makes_search()
+        self.assertIn(
+                    "query=nutella",
+                    self.driver.current_url,
+                    )
     
-    def test_header_title_is_the_query(self):
-        element = self.driver.find_element_by_id("foodsearch")
-        submit = self.driver.find_element_by_id("submit")
-        element.send_keys("nutella")
-        submit.click()
+    def test_results_page_header_title_is_the_query(self):
+        self.user_makes_search()
         header_title = self.driver.find_element_by_id("headerTitle")
         header_title = header_title.text
         self.assertEqual(header_title, "NUTELLA")
 
     
-    def test_when_product_is_selected_details_page_appears(self):
-        element = self.driver.find_element_by_id("foodsearch")
-        submit = self.driver.find_element_by_id("submit")
-        element.send_keys("nutella")
-        submit.click()
-        substitute = self.driver.find_element_by_css_selector("a.substitute")
-        substitute.click()
-        self.assertRegexpMatches(
-                        self.driver.current_url,
-                        r"^http:\//localhost:8000\/substitute\/[0-9]+\/$"
-                        )
+    def test_details_page_retruns_200(self):
+        self.user_makes_search()
+        substitute = self.driver.find_element_by_css_selector("a.substitute") # passer un identfiant unique
+        substitute.click() # attendre après le clique
+        self.wait.until(ec.presence_of_element_located((By.ID, "details")))
+        print(self.driver.current_url)
+        # res = requests.get(self.driver.current_url)
+        self.assertEqual(self.driver.current_url, "http://localhost:8000/substitute/3175681105393/")
     
+    # Faire des tests sur le contenu de la page de détails (image, liste ingrédients, nutriscore)
+
+    # Faire des tests de cas d'exception (pas d'image, pas de liste d'ingrédients) et vérifier que ça s'affiche bien
