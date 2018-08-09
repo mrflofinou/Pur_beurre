1d33cb31c6cb9fba8c11fe52b3a042e65257803b
Florian <florian.lescuyer@gmail.com>
2018-08-09 18:40:59 +0200

    remove template for logout view

--- purbeurre_project/urls.py
+++ purbeurre_project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^substitute/', include('substitute.urls', namespace="substitute")),
     url(r'^signup/$', views.signup, name="signup"),
     url(r'^login/$', auth_views.LoginView.as_view(template_name="substitute/login.html"), name="login"),
-    url(r'^logout/$', auth_views.LogoutView.as_view(template_name="substitute/logout.html"), name="logout"),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page="index"), name="logout"),
     url(r'^admin/', admin.site.urls)
 ]
 
