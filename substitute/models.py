5f70d2c3d7368c34bf71bb43c2d5f5b1478eaf49
Florian <florian.lescuyer@gmail.com>
2018-08-09 18:42:06 +0200

    update models, remove table category and modification of table products

--- substitute/models.py
+++ substitute/models.py
@@ -1,24 +1,17 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField("nom", max_length=200, unique=True)
-
-    class Meta:
-        verbose_name = "catégorie"
-
-    def __str__(self):
-        return self.name
-
 
 class Product(models.Model):
-    name = models.CharField("nom", max_length=200, unique=True)
+    code = models.IntegerField("code produit", unique=True, null=True)
+    name = models.CharField("nom", max_length=200)
     nutriscore = models.CharField("nutriscore", max_length=10)
-    generic_name = models.CharField("nom générique", max_length=200)
-    url_picture = models.CharField("image", max_length=200)
-    categories = models.ManyToManyField(Category,related_name="products", blank=True)
+    url_picture = models.CharField("image", max_length=200, null=True)
+    ingredients = models.CharField("liste d'ingredients", max_length=1000, null=True)
+    url_nutrition = models.CharField("valeurs nutritionnelles", max_length=200, null=True)
+    stores = models.CharField("magasins", max_length=200, null=True)
 
     class Meta:
-        verbose_name = "produit"
+        verbose_name = "produits enregistrés"
 
     def __str__(self):
         return self.name
