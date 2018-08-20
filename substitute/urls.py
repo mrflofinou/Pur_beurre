1721e78c18740b38485f18268b57e2272048d2f1
Florian <florian.lescuyer@gmail.com>
2018-08-20 18:30:26 +0200

    Add urls for save_product and the page of the products of users

--- substitute/urls.py
+++ substitute/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.results, name="results"),
     url(r'^(?P<product_id>[0-9]+)/$', views.details, name="details"),
+    url(r'^ajax/save_product/$', views.save_product, name="save_product"),
+    url(r'^my_products/$', views.my_products, name="my_products")
 ]
