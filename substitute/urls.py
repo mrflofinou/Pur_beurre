785db092222c7087e645aa9090b49850fcd959b2
Florian <florian.lescuyer@gmail.com>
2018-08-28 19:06:49 +0200

    modification of the url for delete a product

--- substitute/urls.py
+++ substitute/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.results, name="results"),
     url(r'^(?P<product_id>[0-9]+)/$', views.details, name="details"),
     url(r'^ajax/save_product/$', views.save_product, name="save_product"),
-    url(r'delete_product/(?P<product_id>[0-9]+)/$', views.delete_product, name="delete_product"),
+    url(r'^ajax/delete_product/$', views.delete_product, name="delete_product"),
     url(r'^my_products/$', views.my_products, name="my_products"),
     url(r'^my_account/$', views.my_account, name="my_account")
 ]
